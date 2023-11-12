917da22b5ec1ff48ba67b31032a2d7b005d08c38
Amirhossein <amirmsh@student.ubc.ca>
2023-11-12 14:50:05 -0800

    Add 3D model and update Hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,15 +18,14 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Amirhossein</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop numerical algorithms in C++ and Python.{" "}
+          Software developer and data enthusiast.{" "}
             <br className="sm:block hidden" />
-            Recently started developing web applications using JavaScript and
-            React.
+            Dive into my projects and discover the world of code, science, and innovation.
           </p>
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
       
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
