38e0921bcffa07f8c3450f1657d8166762e63fd5
Amirhossein <amirmsh@student.ubc.ca>
2023-11-12 17:02:24 -0800

    Adjust screen scale and position in HeroModel component.

--- src/components/canvas/HeroModel.jsx
+++ src/components/canvas/HeroModel.jsx
@@ -17,7 +17,7 @@ const adjustModelForScreenSize = () => {
     screenPosition = [0, -18.5, -23.4];
   } 
    else {
-    screenScale = [8, 8, 8];
+    screenScale = [9.5, 9.5, 9.5];
     screenPosition = [0, -38.5, -50.4];
   }
 
@@ -33,13 +33,8 @@ const HeroModel = () => {
   const [isRotating, setIsRotating] = useState(false);
 
   return (
-    // <section className='w-full h-screen relative'>
+    <section className='w-full h-screen relative'>
     <Canvas
-      // frameloop='demand'
-      // shadows
-      // dpr={[1, 2]}
-      // camera={{ near: 0.1, far: 1000 }}
-      // gl={{ preserveDrawingBuffer: true }}
       className={`${
         isRotating ? "cursor-grabbing" : "cursor-grab"
       }`}
@@ -47,13 +42,6 @@ const HeroModel = () => {
       <Suspense fallback={<CanvasLoader />}>
       <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          {/* <pointLight position={[10, 5, 10]} intensity={2} />
-          <spotLight
-            position={[0, 50, 10]}
-            angle={0.15}
-            penumbra={1}
-            intensity={2}
-          /> */}
           <hemisphereLight
             skyColor='#b1e1ff'
             groundColor='#000000'
@@ -71,7 +59,7 @@ const HeroModel = () => {
 
       <Preload all />
     </Canvas>
-    // </section>
+    </section>
   );
 };
 
