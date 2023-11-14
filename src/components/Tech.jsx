aa0015e90e87ae5056cdeb166e5b9f4c8d0be8c7
Amirhossein <amirmsh@student.ubc.ca>
2023-11-13 17:29:43 -0800

    changed skill block order in app.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,39 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+import { styles } from "../styles";
+
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-        </div>
-      ))}
+    <div className="py-10 flex flex-col">
+      <h3 className={styles.sectionHeadText}>My Skills</h3>
+
+      <div className="mt-16 flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="mt-16 flex flex-wrap gap-12">
+        {technologies.map((technology) => (
+          <div className="block-container w-20 h-20" key={technology.name}>
+            <div className="btn-back rounded-xl" />
+            <div className="btn-front rounded-xl flex justify-center items-center">
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        ))}
+      </div> */}
+
+
     </div>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Tech, "");
